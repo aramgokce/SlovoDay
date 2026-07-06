9b8b6b53db11b71e65db49e477a943b272e41f39
Aram Gokce <aramgokce@gmail.com>
2026-07-06 00:31:45 -0400

    Initial Commit

---
+++ SlovoDay/SlovoDayApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlovoDayApp.swift
+//  SlovoDay
+//
+//  Created by Aram Gokce on 7/6/26.
+//
+
+import SwiftUI
+
+@main
+struct SlovoDayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
